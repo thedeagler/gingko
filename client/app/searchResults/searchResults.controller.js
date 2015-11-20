66c6dbd2e72d9a4e4cbfd739beb4f3d353b0541a
Clayton Schneider <claykschneider@gmail.com>
2015-11-20 11:55:28 -0800

    Removed current multibar from index, to be updated with new one soon

--- client/app/searchResults/searchResults.controller.js
+++ client/app/searchResults/searchResults.controller.js
@@ -10,9 +10,9 @@
   function SearchResultsCtrl(homeFactoryTest, $state, $location, $window) {
 
     this.data = [
-      {name: 'chipotle', picture: 'http://indianapublicmedia.org/eartheats/files/2012/06/5115793807_9e843d90db_b-e1339037423853.jpg', city: 'San Fransisco', state_code: 'CA'},
-      {name: 'subway', picture: 'http://www.restaurantnews.com/wp-content/uploads/2015/08/SUBWAY-Restaurants-Recognizes-Achievements-Of-Outstanding-Franchisees.jpg', city: 'San Fransisco', state_code: 'CA'},
-      {name: 'dominos', picture: 'http://s3.amazonaws.com/newscloud-production/tallmadgeexpress/2014/09/5412b6bc7e85e8c3f4000da9/photos/ken8041364/original.jpg?1410512574', city: 'Menlo Park', state_code: 'CA'},
+      {rating: 3, time: '3:00 PM', name: 'chipotle', picture: 'http://indianapublicmedia.org/eartheats/files/2012/06/5115793807_9e843d90db_b-e1339037423853.jpg', city: 'San Fransisco', state_code: 'CA'},
+      {rating: 2, time: '7:00 PM', name: 'subway', picture: 'http://www.restaurantnews.com/wp-content/uploads/2015/08/SUBWAY-Restaurants-Recognizes-Achievements-Of-Outstanding-Franchisees.jpg', city: 'San Fransisco', state_code: 'CA'},
+      {rating: 4.5, time: '12:00 PM', name: 'dominos', picture: 'http://s3.amazonaws.com/newscloud-production/tallmadgeexpress/2014/09/5412b6bc7e85e8c3f4000da9/photos/ken8041364/original.jpg?1410512574', city: 'Menlo Park', state_code: 'CA'},
     ];
 
     this.randomColor = function() {
