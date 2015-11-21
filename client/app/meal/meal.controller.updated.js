fd91a2eef5db5ad91ac8009cd57e6821a26b748a
Michelle Lee <michelle.mh.lee92@gmail.com>
2015-11-20 21:52:45 -0800

    (feat) Create barebones for meal front-end

---
+++ client/app/meal/meal.controller.updated.js
@@ -0,0 +1,47 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+  .controller('MealCtrl', MealCtrl, MealFactory);
+
+  MealCtrl.$inject = ['$http', '$location', '$window', 'MealFactory'];
+
+  function MealCtrl($scope, $http, $location, $window, Map, MealFactory) {
+    var self = this;
+    self.data = {};
+    var map;
+
+
+
+    
+  }
+
+})();
+
+
+
+// Data
+// - location - neighborhood
+// - location - address
+// - location - city
+// - cll - latitude
+// - cll - longitude
+// - geographical bounding box
+// - name of restaurant
+// - url for photo of restaurant
+// - url for yelp page
+// - display phone number
+// - distance of restaurant from [LOCATION]
+// - URL for star rating
+// - url for large image 
+// - address for restaurant
+// - cross streets for location
+// - reservation url for SeatMe
+
+
+// Other:
+// - Host name
+// - Event time
+// - Attendees
+// - Event description
+
---
+++ client/app/meal/meal.factory.updated.js
@@ -0,0 +1,27 @@
+(function() {
+	'use strict';
+
+	angular.module('app')
+	.factory('MealFactory', MealFactory);
+
+  MealFactory.$inject = ['$http'];
+
+	function MealFactory($http) {
+    var services = {
+      getMeal: getMeal,
+    };
+
+    return services;
+
+    function getMeal(cb) {
+      var path = 'api/in'
+      .then(function success(response) {
+      	cb(response.data);
+      }).catch(function error(response) {
+      	console.log('Unable to retrieve movies');
+      	cb([]);
+      });
+    }
+	}
+
+})();
