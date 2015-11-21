1745cbe3e3e8f3e42c09110e2ebc85adfbe613ce
Mitchell Wilcox <mlwilcox@gmail.com>
2015-11-20 21:56:35 -0800

    (refactor) Add beginning user page styles; displays hard-coded data and user rating stars.

--- client/app/user/user.controller.js
+++ client/app/user/user.controller.js
@@ -4,12 +4,37 @@
   angular.module('app')
   .controller('UserCtrl', UserCtrl);
 
-  UserCtrl.$inject = [];
+  UserCtrl.$inject = ['userFactory'];
 
-  function UserCtrl() {
+  function UserCtrl(userFactory) {
+    
     var self = this;
+ 
+    self.makeStarArr = function(rating) {
+      var fullStarPath = '../../styles/star.png';
+      var halfStarPath = '../../styles/star-half.png';
+      var emptyStarPath = '../../styles/star-empty.png';
+      var starArr = [emptyStarPath, emptyStarPath,
+        emptyStarPath, emptyStarPath, emptyStarPath];
+      var i = 0;
+      while (i < rating) {
+        if (i + 0.5 === rating) {
+          starArr[i]  = halfStarPath;
+        } else {
+          starArr[i] = fullStarPath;
+        }
+        i++;
+      }
+      return starArr;
+    };
 
 
+    userFactory.getUserInfo(function(user){
+      user.hostrating = self.makeStarArr(user.hostrating);
+      user.userrating = self.makeStarArr(user.userrating);
+      self.user = user;
+    });
+  console.log('userrr ' + JSON.stringify(self.user));
   }
 
 })();
