b7e5ede6d6cc918124645a7ddf4c3d642122a24d
Michelle Lee <michelle.mh.lee92@gmail.com>
2015-11-27 23:25:35 -0800

    (feat) Add data binding for Facebook, new mealsData object

--- client/app/meal/meal.controller.js
+++ client/app/meal/meal.controller.js
@@ -8,32 +8,48 @@
 
   function MealCtrl(multibarFactory, $state, $location, $window, $http, mealsData, mealFactory) {
     var self = this;
+    console.log('mealsData =', mealsData);
+    console.log();
 
     self.activate = function() {
-      self.attendees = mealsData.data.attendees;
-      self.mealsData = mealsData.data.meal;
-      self.yelpData = JSON.parse(mealsData.data.meal.yelpData);
-      // console.log('self.attendees =', self.attendees);
-      // console.log('self.mealsData =', self.mealsData);
-      // console.log('self.yelpData =', self.yelpData);
+      self.host = mealsData.data.host;
+      self.meal = mealsData.data.meal;
+      self.restaurant = mealsData.data.restaurant;
+      self.yelpData = mealsData.data.restaurant.yelpData;
+
+      console.log('self.host =', self.host);
+      console.log('self.meal =', self.meal);
+      console.log('self.restaurant =', self.restaurant);
+      console.log('self.yelpData =', self.yelpData);
       self.formatTime();
       self.drawMap();
+      self.getFriends();
     };
 
     self.drawMap = function() {
-      mealFactory.drawMap(Map, self.mealsData.Restaurant.lat, self.mealsData.Restaurant.lng);
+      mealFactory.drawMap(Map, self.yelpData.location.coordinate.latitude, self.yelpData.location.coordinate.longitude);
     };
 
     self.formatTime = function() {
-      var momentDate = moment(self.mealsData.date);
-      var createdAt = self.mealsData.createdAt;
-      self.mealsData.formattedDay = momentDate.format('dddd');
-      self.mealsData.formattedDate = momentDate.format('MMM Do');
-      self.mealsData.formattedTime = momentDate.format('h:mm a');
-      self.mealsData.formattedCreatedAt = moment(createdAt).format('MMM Do');
-      self.mealsData.relativeCreatedAt = moment(createdAt, 'YYYYMMDD').fromNow();
-      self.mealsData.relativeTime = moment(momentDate, 'YYYYMMDD').fromNow();
-      self.mealsData.typeOfMeal = mealFactory.getGreetingTime(momentDate);
+      self.meal.date = new Date(self.meal.date);
+      self.host.updatedAt = new Date(self.host.updatedAt);
+      console.log('self.host.updatedAt =', self.host.updatedAt);
+      var momentDate = moment(self.meal.date);
+      var createdAt = self.host.updatedAt;
+      self.meal.formattedDay = momentDate.format('dddd');
+      self.meal.formattedDate = momentDate.format('MMM Do');
+      self.meal.formattedTime = momentDate.format('h:mm a');
+      self.meal.formattedCreatedAt = moment(createdAt).format('MMM Do');
+      self.meal.relativeCreatedAt = moment(createdAt).fromNow();
+      self.meal.relativeTime = moment(momentDate).fromNow();
+
+      self.meal.typeOfMeal = mealFactory.getGreetingTime(momentDate);
+    };
+
+    self.getFriends = function() {
+      self.host.friendsList = self.host.friends.data.reduce(function(acc, current) {
+        return acc.concat([current.name]);
+      }, []).join(", ");
     };
 
 
