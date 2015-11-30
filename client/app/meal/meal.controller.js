2bb0061c000830605d33443beaa6fe9387f1c814
Michelle Lee <michelle.mh.lee92@gmail.com>
2015-11-30 15:56:55 -0800

    Add Other Events You May Enjoy feature.

--- client/app/meal/meal.controller.js
+++ client/app/meal/meal.controller.js
@@ -15,19 +15,47 @@
 
     self.activate = function() {
       self.host = mealsData.data.host;
+      self.host.gender = _.capitalize(self.host.gender);
       self.meal = mealsData.data.meal;
       self.restaurant = mealsData.data.restaurant;
       self.yelpData = mealsData.data.restaurant.yelpData;
+      self.meal.title = self.meal.title.split(" ").map(function(item) {
+        return _.capitalize(item);
+      }).join(" ");
+      self.meal.description = _.capitalize(self.meal.description.replace('"', ''));
       self.mealId = mealsData.data.meal.id;
 
       self.formatTime();
       self.drawMap();
       self.getFriends();
       self.getAttendees();
+      self.getOtherMeals();
+
       console.log('self.host =', self.host);
       console.log('self.meal =', self.meal);
       console.log('self.restaurant =', self.restaurant);
       console.log('self.yelpData =', self.yelpData);
+      console.log('mealsData =', mealsData);
+
+    };
+
+    self.getOtherMeals = function() {
+      mealFactory.getOtherMeals()
+        .then(function(otherMealData) {
+          self.otherMeals = _.map(otherMealData.data, function(otherMeal, i) {
+            return {
+              id: ++i,
+              title: otherMeal.meal.title.split(" ").map(function(item) {
+                return _.capitalize(item);
+              }).join(" "),
+              address: otherMeal.restaurant.yelpData.location.display_address.join(', '),
+              yelpData: otherMeal.restaurant.yelpData
+            };
+          });
+        })
+        .catch(function(err) {
+          console.log("Error in getting other meal.");
+        });
     };
 
     self.drawMap = function() {
@@ -53,8 +81,8 @@
     };
 
     self.getAttendees = function() {
-      self.meal.attendees = ["Person1", "Person2"]
-    }
+      self.meal.attendees = ["Person1", "Person2"];
+    };
 
 
     self.activate();
--- client/app/meal/meal.factory.js
+++ client/app/meal/meal.factory.js
@@ -1,71 +1,84 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	angular.module('app')
-		.factory('mealFactory', mealFactory);
+  angular.module('app')
+    .factory('mealFactory', mealFactory);
 
-	mealFactory.$inject = ['$http'];
+  mealFactory.$inject = ['$http'];
 
-	function mealFactory($http) {
-		var services = {
-			getGreetingTime: getGreetingTime,
-			drawMap: drawMap,
-			joinMeal: joinMeal
-		};
+  function mealFactory($http) {
+    var services = {
+      getGreetingTime: getGreetingTime,
+      drawMap: drawMap,
+      getOtherMeals: getOtherMeals,
+      joinMeal: joinMeal
+    };
 
-		return services;
+    return services;
 
-		function joinMeal(mealId) {
-			return $http({
-				method: 'PUT',
-				url: '/meals/' + mealId,
-			})
-			.then(function (response) {
-				console.log("successfully posted!", response.data);
-				return response.data;
-			});
-		}
+    function joinMeal(mealId) {
+      return $http({
+          method: 'PUT',
+          url: '/meals/' + mealId,
+        })
+        .then(function(response) {
+          console.log("successfully posted!", response.data);
+          return response.data;
+        });
+    }
 
-		function getGreetingTime(m) {
-			var g;
-			if (!m || !m.isValid()) {
-				return;
-			}
-			var splitMorning = 6;
-			var splitAfternoon = 12;
-			var splitEvening = 18;
-			var currentHour = parseFloat(m.format("HH"));
-			if (currentHour >= splitAfternoon && currentHour <= splitEvening) {
-				g = "Lunch";
-			} else if (currentHour >= splitEvening) {
-				g = "Dinner";
-			} else if (currentHour > splitMorning) {
-				g = "Breakfast";
-			} else {
-				g = "Midnight Snack";
-			}
-			return g;
-		}
+    function getGreetingTime(m) {
+      var g;
+      if (!m || !m.isValid()) {
+        return;
+      }
+      var splitMorning = 6;
+      var splitAfternoon = 12;
+      var splitEvening = 18;
+      var currentHour = parseFloat(m.format("HH"));
+      if (currentHour >= splitAfternoon && currentHour <= splitEvening) {
+        g = "Lunch";
+      } else if (currentHour >= splitEvening) {
+        g = "Dinner";
+      } else if (currentHour > splitMorning) {
+        g = "Breakfast";
+      } else {
+        g = "Midnight Snack";
+      }
+      return g;
+    }
 
-		function drawMap(Map, lat, lng) {
-			var mapCanvas = document.getElementById('map');
-			var myLatLng = {
-			  lat: lat,
-			  lng: lng
-			};
-			var mapOptions = {
-			  center: new google.maps.LatLng(lat, lng),
-			  zoom: 13,
-			  mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
+    function drawMap(Map, lat, lng) {
+      var mapCanvas = document.getElementById('map');
+      var myLatLng = {
+        lat: lat,
+        lng: lng
+      };
+      var mapOptions = {
+        center: new google.maps.LatLng(lat, lng),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
 
-			var map = new google.maps.Map(mapCanvas, mapOptions);
-			var marker = new google.maps.Marker({
-			  position: myLatLng,
-			  title: "hello world!"
-			});
-			marker.setMap(map);
-		}
-	}
+      var map = new google.maps.Map(mapCanvas, mapOptions);
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        title: "hello world!"
+      });
+      marker.setMap(map);
+    }
 
+    function getOtherMeals() {
+      return $http({
+          method: 'GET',
+          url: '/meals'
+        })
+        .then(function success(data) {
+          return data;
+        })
+        .catch(function error(err) {
+          console.log("Error in getting other meals!");
+        });
+    }
+  }
 })();
