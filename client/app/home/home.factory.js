a8fb0396b9a6029c4516f3d81dd5b8b41602724f
Stephen Wu <stefan.wu@gmail.com>
2015-11-30 10:24:30 -0800

    cleaning up for pull request

--- client/app/home/home.factory.js
+++ client/app/home/home.factory.js
@@ -23,52 +23,5 @@
         return response.data;
       });
     }
-<<<<<<< b1391790bcdb6dbc598a6fa67d757251c740ffa5
-=======
-  }
-})();
-
-
-(function() {
-  'use strict';
-
-  angular.module('app')
-  .factory('homeFactoryTest', homeFactoryTest);
-
-  homeFactoryTest.$inject = ['$http'];
-
-  function homeFactoryTest($http) {
-    var services = {
-
-      getMeals : getMeals,
-      getEvent : getEvent
-
-    };
-
-    return services;
-
-    function getEvent (mealID) {
-      return $http({
-        method: 'GET',
-        url: '/meals/' + mealID,
-      })
-      .then(function (response) {
-        return response.data;
-      });
-    }
-
-    function getMeals () {
-      return $http({
-        method: 'GET',
-        url: '/meals'
-      })
-      .then(function (response) {
-        // console.log('meal datas bruuuuhhhh',response)
-        return data;
-      });
-    }
-
-
->>>>>>> (feat) Properly queries database for top rated restaurants
   }
 })();
